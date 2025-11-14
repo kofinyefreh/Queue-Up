71291dcf8fb9d191c8fba3d4deec38007febdce2
Kofi Nyefreh <nyefrehkofi@gmail.com>
2025-11-14 15:24:41 -0800

    complete all media queries for mobile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mainText = document.querySelector('.main-text');
 
 // Auto typing functionality
-const text = 'Organize it all with Queue up One space at a time.';
+const text = 'Organize it all with Queue up — One space at a time';
 let array = [];
 
 function AutoType(text) {
